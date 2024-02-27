6995588c04c702cfe0a0da561f0562d4ab122827
Neverton Sousa <nevertonsousa2@gmail.com>
2024-02-27 15:12:01 -0300

    navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
+// const hamburger = document.querySelector(".hamburger");
+// const nav = document.querySelector(".nav");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+// hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick  = function() {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains( 'open' );
+
+    toggleBtnIcon.classList = isOpen  ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+    }
